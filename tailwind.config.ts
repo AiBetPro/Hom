cb6b2859d8fd33b0c08a44825c0dbe623b2b4092
AiBetPro <agbetomesourou49@gmail.com>
2026-09-24 02:08:51 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,35 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ink: {
+          950: '#0a0e1a',
+          900: '#10162a',
+          800: '#171f38',
+          700: '#1e2745',
+          600: '#2a3459',
+        },
+        gold: {
+          50: '#fbeed9',
+          100: '#f6ddb0',
+          200: '#f0c87e',
+          300: '#eab655',
+          400: '#eaad48',
+          500: '#e8a63d',
+          600: '#c98a28',
+        },
+        cobalt: {
+          400: '#7d95ff',
+          500: '#5170ff',
+        },
+      },
+      fontFamily: {
+        display: ['"Big Shoulders"', 'sans-serif'],
+        body: ['Manrope', 'sans-serif'],
+        data: ['"JetBrains Mono"', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
